fcc848397fee245f94f8091f1551a413705240b9
Ketan Shah <ketan.shah@gmail.com>
2014-10-23 12:26:27 +0530

    renamed template files to tpl. updated route definition

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ var app = angular.module('CustomersApp',['ngResource','ngRoute', 'ui.bootstrap']
 app.config(['$routeProvider',function($routeProvider) {
    $routeProvider
    .when('/list', {
-    templateUrl: 'assets/tpls/customer/list.html',
+    templateUrl: 'assets/tpls/customer/list.tpl',
     controller: 'ListingCtrl'
    })
    .when('/customers/view/:id', {
@@ -12,23 +12,23 @@ app.config(['$routeProvider',function($routeProvider) {
     controller: 'CustomerViewCtrl'
    })
    .when('/leadsourcelist', {
-    templateUrl: 'assets/tpls/leadsource/list.html',
+    templateUrl: 'assets/tpls/leadsource/list.tpl',
     controller: 'LeadsourcetypeslistCtrl'
    })
    .when('/customertypelist', {
-    templateUrl: 'assets/tpls/customertypes/list.html',
+    templateUrl: 'assets/tpls/customertypes/list.tpl',
     controller: 'CustomertypeslistCtrl'
    })
    .when('/titletypelist', {
-    templateUrl: 'assets/tpls/title/list.html',
+    templateUrl: 'assets/tpls/title/list.tpl',
     controller: 'TitlestypeslistCtrl'
    })
    .when('/indutrytypeslist', {
-    templateUrl: 'assets/tpls/industry/list.html',
+    templateUrl: 'assets/tpls/industry/list.tpl',
     controller: 'IndustrytypeslistCtrl'
    })
    .when('/ratingtypeslist', {
-    templateUrl: 'assets/tpls/ratings/list.html',
+    templateUrl: 'assets/tpls/ratings/list.tpl',
     controller: 'RatingtypeslistCtrl'
    })
    .otherwise({
